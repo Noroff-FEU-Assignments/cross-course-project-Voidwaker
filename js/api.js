2a57819582bdfb8e1f8224dbe42520594c2ea071
Voidwaker <128046262+Voidwaker@users.noreply.github.com>
2023-11-01 15:48:41 +0100

    single file for api req

---
+++ js/api.js
@@ -0,0 +1,14 @@
+const url = "https://api.noroff.dev/api/v1/rainy-days";
+
+export async function makeApiCall() {
+    try {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+    } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    }
+}
